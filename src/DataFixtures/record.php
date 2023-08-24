fac89513fc8be2cfc1122a1cb9dca2724465dbec
KaidoOwls <seydina.diallo60@gmail.com>
2023-08-24 16:10:29 +0200

    first commit

---
+++ src/DataFixtures/record.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.1.1deb5ubuntu1
+ */
+
+/**
+ * Database `velvet`
+ */
+
+/* `velvet`.`artist` */
+$artist = array(
+  array('id' => '3','title' => 'Diouf Sabrar','picture' => 'bite','name' => 'Queens Of The Stone Age','url' => 'https://qotsa.com/')
+);
+
+/* `velvet`.`disc` */
+$disc = array(
+  array('id' => '1','title' => 'Songs for the Deaf','picture' => 'https://en.wikipedia.org/wiki/Songs_for_the_Deaf#/media/File:Queens_of_the_Stone_Age_-_Songs_for_the_Deaf.png','artist_id' => '3','label' => 'Interscope Records')
+);
+
+/* `velvet`.`doctrine_migration_versions` */
+$doctrine_migration_versions = array(
+);
+
+/* `velvet`.`messenger_messages` */
+$messenger_messages = array(
+);
