5b79b83d17ddd976712dbf986e6c0196078ada7a
KaidoOwls <seydina.diallo60@gmail.com>
2023-08-25 11:18:22 +0200

    modif

--- src/Form/ContactFormType.php
+++ src/Form/ContactFormType.php
@@ -2,23 +2,36 @@
 
 namespace App\Form;
 
+
 use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ContactFormType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('objet')
             ->add('email')
-            ->add('message')
+
+            //On a rajouté un label et on a rendu le champ optionnel en
+            // donnant la valeur false à l'attribut required
+            ->add('message', TextareaType::class, [
+                'label' => 'Votre message',
+                'required' => false
+                ]
+            )
+            ->add('save', SubmitType::class, [
+                'label' => 'Envoyer le message'])
+
         ;
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => Contact::class,
