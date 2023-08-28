e32920abdcbde40e2badf97615fbd89bdd6ebeaa
KaidoOwls <seydina.diallo60@gmail.com>
2023-08-28 15:26:47 +0200

    modif

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,41 @@
 
 namespace App\Controller;
 
+use App\Repository\ArtistRepository;
+use App\Repository\DiscRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    //On va avoir souvent besoin d'injecter les respositories de nos entités dans les contrôleurs et les services
+    //Pour ne pas les injecter dans chaque fonction, on va les instancier UNE SEULE fois dans le constructeur de notre contrôleur:
+    //N'oubliez pas d'importer vos respositories (les lignes "use..." en haut de la page)
+
+    private $artistRepo;
+    private $discRepo;
+
+    public function __construct(ArtistRepository $artistRepo, DiscRepository $discRepo)
+    {
+        $this->artistRepo = $artistRepo;
+        $this->discRepo = $discRepo;
+
+    }
+
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
+
+        //on appelle la fonction `findAll()` du repository de la classe `Artist` afin de récupérer tous les artists de la base de données;
+
+        $artistes = $this->artistRepo->findAll();
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            //on va envoyer à la vue notre variable qui stocke un tableau d'objets $artistes (c'est-à-dire tous les artistes trouvés dans la base de données)
+            'artistes' => $artistes
         ]);
     }
-} 
+
+}
