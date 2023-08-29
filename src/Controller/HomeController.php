808955b3d8c551eab58fd5e2d787beb0e0c8d3ab
KaidoOwls <seydina.diallo60@gmail.com>
2023-08-29 14:00:15 +0200

    modif

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,31 +2,43 @@
 
 namespace App\Controller;
 
-use App\Repository\ArtistRepository;
 use App\Repository\DiscRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ArtistRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     //On va avoir souvent besoin d'injecter les respositories de nos entités dans les contrôleurs et les services
     //Pour ne pas les injecter dans chaque fonction, on va les instancier UNE SEULE fois dans le constructeur de notre contrôleur:
     //N'oubliez pas d'importer vos respositories (les lignes "use..." en haut de la page)
-
     private $artistRepo;
     private $discRepo;
+    private $em;
 
-    public function __construct(ArtistRepository $artistRepo, DiscRepository $discRepo)
+    public function __construct(ArtistRepository $artistRepo, DiscRepository $discRepo, EntityManagerInterface $em)
     {
         $this->artistRepo = $artistRepo;
         $this->discRepo = $discRepo;
+        $this->em = $em;
 
     }
 
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
+
+
+
+          //on appelle le repository pour accéder à la fonction
+          $artistes = $this->artistRepo->getSomeArtists("Neil");
+
+          //on teste le contenu de la variable $artistes : dd() veut dire Dump and Die
+          dd($artistes); 
+
+       // ...    
 
         //on appelle la fonction `findAll()` du repository de la classe `Artist` afin de récupérer tous les artists de la base de données;
 
