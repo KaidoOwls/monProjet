6c5f759be248e6e3c828048277b6d6cff54e4b12
KaidoOwls <seydina.diallo60@gmail.com>
2023-08-30 16:49:32 +0200

    modif

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,13 +5,17 @@ namespace App\Controller;
 use App\Entity\Contact;
 use App\Form\DemoFormType;
 use App\Form\ContactFormType;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Address;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Validator\Constraints as Assert;
-
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Loader\Configurator\mailer;
 
 
 class ContactController extends AbstractController
@@ -40,6 +44,29 @@ class ContactController extends AbstractController
             // Redirection vers accueil
             return $this->redirectToRoute('app_home');
         }
+
+        $email = (new TemplatedEmail())
+        ->from('hello@example.com')
+//            ->to('you@example.com')
+        ->to(new Address('ryan@example.com'))
+        //->cc('cc@example.com')
+        //->bcc('bcc@example.com')
+        //->replyTo('fabien@example.com')
+        //->priority(Email::PRIORITY_HIGH)
+        ->subject('Time for Symfony Mailer!')
+
+        // le chemin de la vue Twig à utiliser dans le mail
+        ->htmlTemplate('emails/signup.html.twig')
+
+        // un tableau de variable à passer à la vue; 
+       //  on choisit le nom d'une variable pour la vue et on lui attribue une valeur (comme dans la fonction `render`) :
+        ->context([
+                'expiration_date' => new \DateTime('+7 days'),
+                'username' => 'foo',
+            ]);
+
+    
+
 
         return $this->render('contact/index.html.twig', [
 //            'form' => $form->createView(),
