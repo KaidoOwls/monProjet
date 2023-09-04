40b7c8ce4b0e0b9dd45d82dd70d8c7b6a2bbc76c
KaidoOwls <seydina.diallo60@gmail.com>
2023-09-04 15:10:49 +0200

    modif

--- src/Service/MailService.php
+++ src/Service/MailService.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Service;
+
+use Symfony\Component\BrowserKit\Response;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 //...
 
@@ -14,13 +16,14 @@ class MailService
         $this->paramBag = $paramBag;
     }
 
-    public function sendMail($expediteur, $destinataire, $sujet, $message){
-
+    public function sendMail($expediteur, $destinataire, $sujet, $message)
+    {
+        $dossiers_images = $this->paramBag->get('images_directory');
     }
 //On se sert du parameterBag et du nom du paramètre ('image_directory') pour récupèrer le chemin du dossier "images"
         
         
-        $dossiers_images = $this->paramBag->get('images_directory');
+
         
 //...
 
