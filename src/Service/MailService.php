27b75d8cb958b0878eef2b10cb9c7a0e057ae8aa
KaidoOwls <seydina.diallo60@gmail.com>
2023-09-05 16:45:21 +0200

    modif

--- src/Service/MailService.php
+++ src/Service/MailService.php
@@ -13,15 +13,14 @@ class MailService
 
     public function __construct(ParameterBagInterface $paramBag)
     {
-        $this->paramBag = $paramBag;
+        $this->paramBag = $paramBag; 
     }
 
     public function sendMail($expediteur, $destinataire, $sujet, $message)
     {
         // On se sert du parameterBag et du nom du paramètre ('images_directory') pour récupérer le chemin du dossier "images"
         $dossiers_images = $this->paramBag->get('images_directory');
-
         // Vous pouvez maintenant utiliser $dossiers_images dans votre méthode pour le traitement nécessaire.
-        // ...
+        // ... j'ai fais une moodif
     }
 }
